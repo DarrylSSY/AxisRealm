f5fa2d0c24c1fe82e2ca3d0daae885249f883543
BuildTools <unconfigured@null.spigotmc.org>
2018-07-13 01:21:54 +0800

    Alpha V2

---
+++ plugins/dynmap/web/standalone/MySQL_config.php
@@ -0,0 +1,9 @@
+<?php
+$dbname = 'axismap';
+$dbhost = 'localhost';
+$dbport = 3306;
+$dbuserid = 'axismap';
+$dbpassword = 'AxisRealm69420';
+$dbprefix = 'dyn_';
+$loginenabled = false;
+?>
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Xms5G -Xmx5G -XX:+UseConcMarkSweepGC -Djava.awt.headless=true -jar spigot.jar
